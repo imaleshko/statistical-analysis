6db448dbca29d507b32a94dfc7ff8f9296b1dc8a
ivan_maleshko <maleshko.ivan@student.uzhnu.edu.ua>
2026-05-09 01:02:59 +0300

    feat: deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from "path";
 
 export default defineConfig({
   plugins: [react()],
+  base: "/statistical-analysis/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
